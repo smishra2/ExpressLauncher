c9af25d3e073ceab4be7260a34078d4f4f948882
Sachit Mishra <smishra2@ufl.edu>
2015-01-04 16:36:48 -0500

    Got rid of redundant animation code

--- app/src/main/java/com/sachitmishra/expresslauncher/activity/AppDrawer.java
+++ app/src/main/java/com/sachitmishra/expresslauncher/activity/AppDrawer.java
@@ -28,7 +28,6 @@ public class AppDrawer extends Activity {
     public void onBackPressed() {
         Intent intent = new Intent(this, HomeActivity.class);
         startActivity(intent);
-        overridePendingTransition(R.anim.slide_up, R.anim.slide_down);
     }
 
 }
--- app/src/main/java/com/sachitmishra/expresslauncher/activity/GesturesList.java
+++ app/src/main/java/com/sachitmishra/expresslauncher/activity/GesturesList.java
@@ -3,9 +3,15 @@ package com.sachitmishra.expresslauncher.activity;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.gesture.GestureLibraries;
+import android.gesture.GestureLibrary;
 import android.os.Bundle;
+import android.widget.ListView;
 
 import com.sachitmishra.expresslauncher.R;
+import com.sachitmishra.expresslauncher.adapter.GestureAdapter;
+
+import java.io.File;
 
 /**
  * Created by Sachit on 1/4/2015.
@@ -17,12 +23,17 @@ public class GesturesList extends Activity {
         super.onCreate(savedInstanceState);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         setContentView(R.layout.gestures_list);
+
+        ListView gesturesList = (ListView) findViewById(R.id.gestures_listview);
+        gesturesList.setEmptyView(findViewById(R.id.empty_gestures));
+        File gesfile = new File(this.getFilesDir() + File.separator + "GestureLibrary");
+        GestureLibrary gestureLibrary = GestureLibraries.fromFile(gesfile);
+        gesturesList.setAdapter(new GestureAdapter(this, gestureLibrary));
     }
 
     @Override
     public void onBackPressed() {
         Intent intent = new Intent(this, HomeActivity.class);
         startActivity(intent);
-        overridePendingTransition(R.anim.slide_up, R.anim.slide_down);
     }
 }
